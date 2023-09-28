efab365a94b3e37b31602c1426761f424143904e
Fuyao Zhao <zfy0701@gmail.com>
2023-09-27 19:08:33 -0700

    initial add sentio traces
    util script to push image
    
    fix tracer condition check
    
    intial supporting debug_tracecallmany
    
    don't expose internal method
    
    reuse getBlockByArg rather than own imp
    
    implement trace call
    
    fix serialization
    
    fix tracecall decoding, default signature, etc
    
    fix return format
    
    polygzkevm doesn't have can transfer
    
    update push script
    
    fix build
    
    add more info to root trace
    
    don't set block number on tracer
    
    set transfer value to zero if can't transfer
    
    temp fix
    
    Add Mapping keys to post account
    
    fail the trace call if estimate gas failed
    
    migrate tracer changes

---
+++ push-image.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+export DOCKER_DEFAULT_PLATFORM=linux/amd64
+
+make build-docker
+docker tag x1-node:latest ${SENTIO_DOCKER_REPO}/x1-node:v0.2.0
+docker push ${SENTIO_DOCKER_REPO}/x1-node:v0.2.0
