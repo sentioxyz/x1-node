a1fa22e34a8aca30680c637170fcd0e39e3d3ac6
Fuyao Zhao <zfy0701@gmail.com>
2024-03-14 11:50:43 -0700

    patch push script

--- push-image.sh
+++ push-image.sh
@@ -3,5 +3,5 @@
 export DOCKER_DEFAULT_PLATFORM=linux/amd64
 
 make build-docker
-docker tag zkevm-node:latest ${SENTIO_DOCKER_REPO}/zkevm-node:develop
-docker push ${SENTIO_DOCKER_REPO}/zkevm-node:develop
+docker tag x1-node:latest ${SENTIO_DOCKER_REPO}/x1-node:v0.3.0
+docker push ${SENTIO_DOCKER_REPO}/x1-node:v0.3.0
